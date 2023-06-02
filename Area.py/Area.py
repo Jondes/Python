6ade85e45ea9bc18ecc6446b131fafa81e7b0a93
Johnson Guedes <jondes560@hotmail.com>
2023-06-02 10:50:09 +0100

    Area commit

---
+++ Area.py/Area.py
@@ -0,0 +1,10 @@
+def calculatearea(width, height):
+ return width * height
+
+print('Height: ') 
+a = int(input())
+
+print('Weight: ') 
+b = int(input())
+
+print('The area is :', calculatearea( a, int(b)))
