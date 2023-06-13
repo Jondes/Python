03f0982d2303e934ae7c37b86a129fee1f3515c5
Johnson Guedes <jondes560@hotmail.com>
2023-06-13 10:56:03 +0100

    AND commit

---
+++ AND.py
@@ -0,0 +1,6 @@
+answer = input('What is the answer ? a,b,c,d :\n')
+
+while answer != 'a' and answer!= 'b' and answer!= 'c' and answer!= 'd':
+    answer = input('Please enter a,b,c,d')
+
+print('Your choice', answer)    
