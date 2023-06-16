1ef3f00c357ebe547a2463ae58575144ebd6e8a9
Johnson Guedes <jondes560@hotmail.com>
2023-06-16 07:40:32 +0100

    Strings commit

---
+++ Strings.py
@@ -0,0 +1,23 @@
+course = 'Python for Beginners'
+print(course[0:])
+print(course[0:1])
+print(course[0:5])
+print(course[-1:])
+print(course[0:])
+print(course[1:-1])
+print(len(course))
+print(course.upper())
+print(course.lower ())
+print(course.find('beginners'))
+print(course.replace('beginners', 'Absolute beginners'))
+print(course.replace('B', 'J'))
+print('Python' in course)
+print('python' in course)
+
+first = 'John'
+last = 'Smith'
+msg = f'{first} {last} is a coder'
+print(msg)
+
+
+
